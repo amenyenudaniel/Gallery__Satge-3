dbb8e301641625551fc6aa43def3573fdbb60c56
amenyenudaniel <amenyenudaniel321@gmail.com>
2023-09-21 09:11:42 +0000

    Search Functionality is done

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchMenu, setSearchMenu] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -24,19 +25,48 @@ const SearchBar = () => {
   };
 
   return (
-    <form className="searchBar" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search..."
-        onChange={handleChange}
-        value={searchTerm}
-      />
-      <img
-        src={SearchIcon}
-        alt="search-icon"
-        onClick={() => handleSearch(searchTerm)}
-      />
-    </form>
+    <>
+      <form className="searchBar" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={handleChange}
+          value={searchTerm}
+        />
+        <img
+          className="searchDesktop"
+          src={SearchIcon}
+          alt="search-icon"
+          onClick={() => handleSearch(searchTerm)}
+        />
+
+        <img
+          src={SearchIcon}
+          className="searchMobile"
+          alt="search-icon"
+          onClick={() => setSearchMenu(true)}
+        />
+      </form>
+      {searchMenu && (
+        <div className="app__mobile slide-bottom">
+          <form className="app__search" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search..."
+              onChange={handleChange}
+              value={searchTerm}
+            />
+            <img
+              src={SearchIcon}
+              alt="search-icon"
+              className="mobile__icon-search"
+              onClick={() => handleSearch(searchTerm)}
+            />
+            <p onClick={() => setSearchMenu(false)}>&times;</p>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
